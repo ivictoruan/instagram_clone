2ec395ee84d6e5660004f20b71662acfb303a8eb
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2022-01-26 09:26:06 -0300

    Criando widget TextFieldInput!

---
+++ lib/widgets/text_field_input.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TextFieldInput extends StatelessWidget {
+  const TextFieldInput({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      
+    );
+  }
+}
