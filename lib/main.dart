ded0a28201412825006b8bacba4b2c25f931c837
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2022-01-26 08:45:28 -0300

    Terminando configuração de inicialização do firebase com options

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive/web_screen_layout.dart';
 import 'package:instagram_clone/utils/colors.dart';
-
 import 'responsive/responsive_layout_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if(kIsWeb){
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyC8PxFWUhcDncaOWhKVsfQaoPmO-AjqBdE", 
+        appId: "1:233039097678:web:02e07ee01758e6f204f81f", 
+        messagingSenderId: "233039097678", 
+        projectId: "instagram-clone-15ca4",
+        storageBucket: "instagram-clone-15ca4.appspot.com",
+        ),
+    );
+  }else{
+  await Firebase.initializeApp(); // Initialize Firebase
+  }
   runApp(const MyApp());
 }
 
@@ -23,7 +38,10 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: mobileBackgroundColor,
       ),      
       
-      home: const ResponsiveLayout(mobileScreenLayout: MobileScreenLayout(), webScreenLayout: WebScreenLayout(),),
+      home: const ResponsiveLayout(
+        mobileScreenLayout: MobileScreenLayout(), 
+        webScreenLayout: WebScreenLayout()
+      ),
     );
   }
 }
