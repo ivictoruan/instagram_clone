0a804ceb287158c481aa25147ccce34be9c21d8b
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2022-02-01 15:16:50 -0300

    Direcionando tela inicial de acordo com os estados de cada usuário no Firebase. Se estiver logado, se estiver carregando, se deu erro ou se não está registrado!

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
+import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
+import 'package:instagram_clone/responsive/web_screen_layout.dart';
 import 'package:instagram_clone/screens/login_screen.dart';
 import 'package:instagram_clone/utils/colors.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if(kIsWeb){
+  if (kIsWeb) {
     await Firebase.initializeApp(
       options: const FirebaseOptions(
-        apiKey: "AIzaSyC8PxFWUhcDncaOWhKVsfQaoPmO-AjqBdE", 
-        appId: "1:233039097678:web:02e07ee01758e6f204f81f", 
-        messagingSenderId: "233039097678", 
+        apiKey: "AIzaSyC8PxFWUhcDncaOWhKVsfQaoPmO-AjqBdE",
+        appId: "1:233039097678:web:02e07ee01758e6f204f81f",
+        messagingSenderId: "233039097678",
         projectId: "instagram-clone-15ca4",
         storageBucket: "instagram-clone-15ca4.appspot.com",
-        ),
+      ),
     );
-  }else{
-  await Firebase.initializeApp(); // Initialize Firebase
+  } else {
+    await Firebase.initializeApp(); // Initialize Firebase
   }
   runApp(const MyApp());
 }
@@ -31,16 +35,27 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Instagram',
-
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: mobileBackgroundColor,
-      ),      
-      
-      // home: const ResponsiveLayout(
-      //   mobileScreenLayout: MobileScreenLayout(), 
-      //   webScreenLayout: WebScreenLayout()
-      // ),
-      home: const LoginScreen(),
+      ),
+      home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.active) { // se o usuário está logado
+              if (snapshot.hasData) { // usuário está autenticado
+                return const ResponsiveLayout(
+                    mobileScreenLayout: MobileScreenLayout(),
+                    webScreenLayout: WebScreenLayout());
+              } else if (snapshot.hasError) { // deu erro
+                return Center(child: Text('${snapshot.error}'));
+              }
+            }
+            if (snapshot.connectionState == ConnectionState.waiting) { // esperando conexão
+              return const Center(
+                  child: CircularProgressIndicator(color: primaryColor));
+            }
+          return const LoginScreen(); // usuário não está autenticado
+          }),
     );
   }
 }
