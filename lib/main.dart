7630e315a4d93c5d9670507ab566c296df760f50
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2022-02-02 10:18:09 -0300

    Colando um MultiProvider na camada acima do MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/providers/user_provider.dart';
 import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
 import 'package:instagram_clone/responsive/web_screen_layout.dart';
 import 'package:instagram_clone/screens/login_screen.dart';
 import 'package:instagram_clone/utils/colors.dart';
+import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -32,30 +34,35 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Instagram',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: mobileBackgroundColor,
-      ),
-      home: StreamBuilder(
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.active) { // se o usuário está logado
-              if (snapshot.hasData) { // usuário está autenticado
-                return const ResponsiveLayout(
-                    mobileScreenLayout: MobileScreenLayout(),
-                    webScreenLayout: WebScreenLayout());
-              } else if (snapshot.hasError) { // deu erro
-                return Center(child: Text('${snapshot.error}'));
+    return MultiProvider(     
+      providers: [
+        ChangeNotifierProvider(create: (_) => UserProvider()),
+      ],      
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Instagram',
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: mobileBackgroundColor,
+        ),
+        home: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.active) { // se o usuário está logado
+                if (snapshot.hasData) { // usuário está autenticado
+                  return const ResponsiveLayout(
+                      mobileScreenLayout: MobileScreenLayout(),
+                      webScreenLayout: WebScreenLayout());
+                } else if (snapshot.hasError) { // deu erro
+                  return Center(child: Text('${snapshot.error}'));
+                }
               }
-            }
-            if (snapshot.connectionState == ConnectionState.waiting) { // esperando conexão
-              return const Center(
-                  child: CircularProgressIndicator(color: primaryColor));
-            }
-          return const LoginScreen(); // usuário não está autenticado
-          }),
+              if (snapshot.connectionState == ConnectionState.waiting) { // esperando conexão
+                return const Center(
+                    child: CircularProgressIndicator(color: primaryColor));
+              }
+            return const LoginScreen(); // usuário não está autenticado
+            }),
+      ),
     );
   }
 }
