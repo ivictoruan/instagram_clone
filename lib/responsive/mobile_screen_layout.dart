f6eacc251a86d0ab85a8ed721d71f324a71f0c3d
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2022-02-02 12:31:08 -0300

    Fazendo parte de baixo da tela (BottonNavigationbar) assim como implementando a lógica de troca destas telas  por meio de um controller

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/colors.dart';
 
 class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({Key? key}) : super(key: key);
@@ -8,11 +10,77 @@ class MobileScreenLayout extends StatefulWidget {
 }
 
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
- 
+  int _page = 0;
+  late PageController pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController();    
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
+  }
+
+  void navigationTapped (int page){
+    pageController.jumpToPage(page);
+  }
+
+  void onPageChanged (int page){
+    setState(() {
+      _page = page;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("This is mobile layout")),
+    return Scaffold(
+      body: PageView(
+        children: const [
+          Text("feed"),
+          Text("search"),
+          Text("add post"),
+          Text("notifications"),
+          Text("profile"),
+
+        ],
+        controller: pageController,
+        onPageChanged: onPageChanged,
+        physics: const NeverScrollableScrollPhysics(), // p/ a tela não passar deslisando lateralmente
+      ),
+      bottomNavigationBar:
+          CupertinoTabBar(backgroundColor: mobileBackgroundColor, items: [
+        BottomNavigationBarItem(
+            icon: Icon(Icons.home,
+                color: _page == 0 ? primaryColor : secondaryColor),
+            label: "",
+            backgroundColor: primaryColor),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.search,
+                color: _page == 1 ? primaryColor : secondaryColor),
+            label: "",
+            backgroundColor: primaryColor),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle,
+                color: _page == 2 ? primaryColor : secondaryColor),
+            label: "",
+            backgroundColor: primaryColor),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.favorite,
+                color: _page == 3 ? primaryColor : secondaryColor),
+            label: "",
+            backgroundColor: primaryColor),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.person,
+                color: _page == 4 ? primaryColor : secondaryColor),
+            label: "",
+            backgroundColor: primaryColor),
+      ],
+      onTap: navigationTapped,
+      ),
     );
   }
 }
