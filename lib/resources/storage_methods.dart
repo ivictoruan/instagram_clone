e885d60d6baccd48eac2d59c5c350d8e179bf337
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2022-02-04 15:09:16 -0300

    Ao fazer upload de uma imagem, verifica se já foi postado para gerar um id único por post

--- lib/resources/storage_methods.dart
+++ lib/resources/storage_methods.dart
@@ -2,6 +2,8 @@ import 'dart:typed_data';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:uuid/uuid.dart';
+
 
 
 class StorageMethods{
@@ -12,6 +14,11 @@ class StorageMethods{
   Future<String> uploadImageToStorage(String childName, Uint8List file, bool isPost) async {
   
     Reference ref = _storage.ref().child(childName).child(_auth.currentUser!.uid);
+
+    if(isPost) { // se estiver postado, gera um id único para o post
+      String id = const Uuid().v1();
+      ref = ref.child(id);
+    }
 
     UploadTask uploadTask = ref.putData(file);
 
