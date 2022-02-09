8c7797c224144c9347d638e1b19bdfe3ed946028
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2022-02-09 12:28:33 -0300

    Criando tela de comentários

---
+++ lib/screens/comments_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/widgets/comment_card.dart';
+
+class CommentsScreen extends StatefulWidget {
+  const CommentsScreen({Key? key}) : super(key: key);
+
+  @override
+  _CommentsScreenState createState() => _CommentsScreenState();
+}
+
+class _CommentsScreenState extends State<CommentsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Comentários"),
+        centerTitle: false,
+        backgroundColor: mobileBackgroundColor,
+      ),
+      body: CommentCard(),
+      bottomNavigationBar: SafeArea(
+        child: Container(
+          height: kToolbarHeight,
+          margin: EdgeInsets.only(
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+          ),
+          padding: const EdgeInsets.only(
+            left: 16,
+            right: 8,
+          ),
+          child: Row(
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(
+                  "https://images.unsplash.com/photo-1644241937671-ee6c92b5ee2f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+                ),
+                radius: 18,
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    left: 16,
+                    right: 8,
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: " Comentar como \$username",
+                      border: InputBorder.none
+                    ),
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: (){},
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 8,
+                  ),
+                  child: const Text(
+                    "Postar",
+                    style: TextStyle(
+                      color: blueColor,
+                    ),
+                  ),
+                ),
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
