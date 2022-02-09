8b3ffe0fa757adeda26f7ba20bc7b9a962db61f2
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2022-02-09 12:29:01 -0300

    criando card de comentários

---
+++ lib/widgets/comment_card.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/colors.dart';
+
+class CommentCard extends StatefulWidget {
+  const CommentCard({Key? key}) : super(key: key);
+
+  @override
+  _CommentCardState createState() => _CommentCardState();
+}
+
+class _CommentCardState extends State<CommentCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        vertical: 16,
+        horizontal: 16,
+      ),
+      child: Row(
+        children: [
+          const CircleAvatar(
+            backgroundImage: NetworkImage(
+              "https://images.unsplash.com/photo-1644347572455-9792032705de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
+            ),
+            radius: 18,
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                left: 16,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  RichText(
+                    text: const TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "username",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, color: primaryColor),
+                        ),
+                        TextSpan(
+                          text: " Alguma descrição para inserir",
+                          style: TextStyle(
+                            color: primaryColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(
+                      top: 4,
+                    ),
+                    child: Text(
+                      "01/01/01",
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: const Icon(
+              Icons.favorite,
+              size: 16,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
